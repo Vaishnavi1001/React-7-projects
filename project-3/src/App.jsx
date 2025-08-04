353df920e37a63886ed2d8ce2f5067dc8fb1a0a6
Vaishnavi1001 <vaishnavivg15@gmail.com>
2025-08-04 15:30:57 +0530

    add some files

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -2,20 +2,16 @@ import { useState } from "react";
 import StartGame from "./components/StartGame";
 import GamePlay from "./components/GamePlay";
 
-
 function App() {
-    
-    const[isGameStarted,setIsGameStarted] = useState(true);
-    const toggleGamePlay = () =>{
-        setIsGameStarted((prev)=>!prev);
-    }
-    return(
-        <>
-        {isGameStarted ? <GamePlay/>:<StartGame toggle={toggleGamePlay}/>}
-        
-        </>
-    );
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  };
+
+  return (
+    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
+  );
 }
 
 export default App;
-
--- project-3/src/components/StartGame.jsx
+++ project-3/src/components/StartGame.jsx
@@ -1,16 +1,16 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { Button } from "../styled/Button";
 
-
-const StartGame = ({toggle}) => {
+const StartGame = ({ toggle }) => {
   return (
     <Container>
-        <div>
-    <img src='images/dices.png'/></div>
-    <div className='content'>
+      <div>
+        <img src="/images/dices.png" />
+      </div>
+      <div className="content">
         <h1>Dice Game</h1>
-        <Button
-        onClick={toggle}>Play Now</Button>
-    </div>
+        <Button onClick={toggle}>Play Now</Button>
+      </div>
     </Container>
   );
 };
@@ -18,35 +18,16 @@ const StartGame = ({toggle}) => {
 export default StartGame;
 
 const Container = styled.div`
-    max-width: 1180px;
-    height: 100vh;
-    display: flex;
-    margin: 0 auto;
-    align-items: center;
-    .content{
-        h1{
-            font-size: 96px;
-            white-space: nowrap;
-        }
-    }
-`;
-const Button = styled.button`
-    padding: 10px 18px;
-    background: #000000;
-    border-radius: 5px;
-    color: white;
-    min-width: 220px;
-    border: none;
-    font-size: 16px;
-    border: 1px solid transparent;
-    transition: 0.4s background ease-in;
-    cursor: pointer;
+  max-width: 1180px;
+  height: 100vh;
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
 
-    &:hover{
-        background-color: white;
-        border: 1px solid black;
-        color: black;
-        transition: 0.3s background ease-in;
+  .content {
+    h1 {
+      font-size: 96px;
+      white-space: nowrap;
     }
-
+  }
 `;
